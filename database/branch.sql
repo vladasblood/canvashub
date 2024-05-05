47363849b2081978de4ddec6466a9ba862768429
vladasblood <vladimirguerrero22@gmail.com>
2024-05-05 17:43:25 +0800

    database-folder-updated

---
+++ database/branch.sql
@@ -0,0 +1,6 @@
+INSERT INTO `branch` (`branch_id`, `address_id`, `store_id`)
+VALUES
+    ('BID1', 'AID1', 'SID1'),
+    ('BID2', 'AID2', 'SID2'),
+    ('BID3', 'AID3', 'SID3'),
+    ('BID4', 'AID4', 'SID4');
